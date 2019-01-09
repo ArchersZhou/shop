7387102defd52d236fa2147244ce5c1bfd995a8a
archerzhou <861769878@qq.com>
2019-01-09 19:50:29 +0800

    houtai

--- web/app/Http/Controllers/Admin/IndexController.php
+++ web/app/Http/Controllers/Admin/IndexController.php
@@ -47,4 +47,108 @@ class IndexController extends Controller
     public function Amounts(){
         return view('admin/Amounts');
     }
+    public function article_add(){
+        return view('admin/article_add');
+    }
+    public function article_list(){
+        return view('admin/article_list');
+    }
+    public function article_Sort(){
+    return view('admin/article_Sort');
+    }
+    public function Brand_detailed(){
+        return view('admin/Brand_detailed');
+    }
+    public function Brand_Manage(){
+        return view('admin/Brand_Manage');
+    }
+    public function Category_Manage(){
+        return view('admin/Category_Manage');
+    }
+    public function Competence(){
+        return view('admin/Competence');
+    }
+    public function Cover_management(){
+        return view('admin/Cover_management');
+    }
+    public function Feedback(){
+        return view('admin/Feedback');
+    }
+    public function Guestbook(){
+        return view('admin/Guestbook');
+    }
+    public function integration(){
+        return view('admin/integration');
+    }
+    public function member_Grading(){
+        return view('admin/member_Grading');
+    }
+    public function member_show(){
+        return view('admin/member_show');
+    }
+    public function Order_Chart(){
+        return view('admin/Order_Chart');
+    }
+    public function order_detailed(){
+        return view('admin/order_detailed');
+    }
+    public function Order_handling(){
+        return view('admin/Order_handling');
+    }
+    public function Orderform(){
+    return view('admin/Orderform');
+    }
+    public function Payment_Configure(){
+        return view('admin/Payment_Configure');
+    }
+    public function Payment_details(){
+        return view('admin/Payment_details');
+    }
+    public function payment_method(){
+        return view('admin/payment_method');
+    }
+    public function picture_add(){
+        return view('admin/picture_add');
+    }
+    public function product_category_add(){
+        return view('admin/product_category_add');
+    }
+    public function Products_List(){
+        return view('admin/Products_List');
+    }
+    public function Refund(){
+        return view('admin/Refund');
+    }
+    public function Refund_detailed(){
+        return view('admin/Refund_detailed');
+    }
+    public function Shop_list(){
+        return view('admin/Shop_list');
+    }
+    public function shopping_detailed(){
+        return view('admin/shopping_detailed');
+    }
+    public function Shops_Audit(){
+        return view('admin/Shops_Audit');
+    }
+    public function Sort_ads(){
+        return view('admin/Sort_ads');
+    }
+    public function System_Logs(){
+        return view('admin/System_Logs');
+    }
+    public function System_section(){
+        return view('admin/System_section');
+    }
+    public function Systems(){
+        return view('admin/Systems');
+    }
+    public function transaction(){
+        return view('admin/transaction');
+    }
+    public function user_list(){
+        return view('admin/user_list');
+    }
+
+
 }
