c7fa43be284438e466bd17e5d70f8750586f41e0
archerzhou <861769878@qq.com>
2019-01-09 12:04:58 +0800

    lail

---
+++ web/resources/views/login.blade.php
@@ -0,0 +1,33 @@
+<!doctype html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>后台管理</title>
+    <link href="/month11/web/public/css/admin_login.css" rel="stylesheet" type="text/css" />
+</head>
+<body>
+<div class="admin_login_wrap">
+    <h1>后台管理</h1>
+    <div class="adming_login_border">
+        <div class="admin_input">
+            <form action="" method="post">
+                <ul class="admin_items">
+                    <li>
+                        <label for="user">用户名：</label>
+                        <input type="text" name="name" id="user" size="30" class="admin_input_style" />
+                    </li>
+                    <li>
+                        <label for="pwd">密码：</label>
+                        <input type="password" name="pwd" id="pwd" size="30" class="admin_input_style" />
+                    </li>
+                    <li>
+                        <input type="submit" tabindex="3" value="提交" class="btn btn-primary" />
+                    </li>
+                </ul>
+            </form>
+        </div>
+    </div>
+    <p class="admin_copyright"><a href="">没有账号，注册一个吧</a></p>
+</div>
+</body>
+</html>
